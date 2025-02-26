916cc368bedeade2efcec81fa513cc26cd4868e1
EduardoBaptista01 <e.f.baptista@hotmail.com>
2025-02-26 14:06:54 -0500

    lint adjustments

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -33,10 +33,10 @@ def bad_request(error):
     return (
         jsonify(
             status=status.HTTP_400_BAD_REQUEST,
-             error="Bad Request", 
-             message=message
+            error="Bad Request",
+            message=message
         ),
-        status.HTTP_400_BAD_REQUEST,
+        status.HTTP_400_BAD_REQUEST
     )
 
 
@@ -47,7 +47,7 @@ def not_found(error):
     app.logger.warning(message)
     return (
         jsonify(
-            status=status.HTTP_404_NOT_FOUND, 
+            status=status.HTTP_404_NOT_FOUND,
             error="Not Found",
             message=message),
         status.HTTP_404_NOT_FOUND
